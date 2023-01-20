22cb4c975e4c1f7f4f4c17e9d994b865bc2e49ee
Daniel Njuguna <kagudante@gmail.com>
2023-01-20 17:13:39 +0300

    added app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [imgSrc, setImgSrc] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((data) => setImgSrc(data.message))
+    }, [])
+
+    if (!imgSrc) return <p>Loading...</p>
+
+    return (
+        <img src={imgSrc} alt="A Random Dog"></img>
+    )
+
+}
+
+export default App;
